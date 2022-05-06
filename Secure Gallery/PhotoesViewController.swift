6b54e58da53e25c7a0978d73320c2f21ed5925af
Артём Симан <neonx@MacBook-Air-Artem.local>
2022-05-06 12:43:39 +0300

    Initial commit

--- Secure Gallery/PhotoesViewController.swift
+++ Secure Gallery/PhotoesViewController.swift
@@ -6,10 +6,14 @@
 //
 
 import UIKit
+import Kingfisher
+import SwiftUI
+import Alamofire
 
 class PhotoesViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
+    var imageArray = FileStorage.getAllImages()
     
     @IBAction func addPhotoButton(_ sender: UIButton) {
         let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
@@ -18,6 +22,9 @@ class PhotoesViewController: UIViewController {
         })
         alert.addAction(UIAlertAction(title: "Camera", style: .default) { [weak self] _ in
             self?.openCamera()
+        })
+        alert.addAction(UIAlertAction(title: "Internet", style: .default) { [weak self] _ in
+            self?.openInternet()
         })
         
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
@@ -39,19 +46,19 @@ class PhotoesViewController: UIViewController {
 
 extension PhotoesViewController: UICollectionViewDataSource, UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return imageBase.images.count
+        return imageArray.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! PhotoCollectionViewCell
-        let photoes = imageBase.images[indexPath.row]
+        let photoes = imageArray[indexPath.row]
         cell.setupCell(pic: photoes)
         return cell
     }
   
 }
 
-private extension UIViewController {
+private extension PhotoesViewController {
     func openGallery() {
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.photoLibrary) {
             let imagePicker = UIImagePickerController()
@@ -79,13 +86,53 @@ private extension UIViewController {
             self.present(alert, animated: true, completion: nil)
         }
     }
+
+    func openInternet() {
+        let alertController = UIAlertController(title: "Write a link to download", message: nil, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Download", style: .default) {_ in
+            if let textField = alertController.textFields?.first, let text = textField.text {
+                self.downloadPicture(pictureUrl: text)
+            }
+        })
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alertController.addTextField {
+            (textField) in
+            textField.placeholder = "Write URL"
+        }
+        
+        present(alertController, animated: true)
+        
+    }
+    
+    func downloadPicture(pictureUrl: String) {
+        AF.request( pictureUrl, method: .get, parameters: nil).responseData {
+            [weak self] response in
+            if let status = response.response?.statusCode {
+                guard (200..<300).contains(status) else {
+                    print("Wrong response status: \(status)")
+                    return
+                }
+                let url = URL(string: "\(pictureUrl)")!
+                DispatchQueue.global().async {
+                    if let data = try? Data(contentsOf: url) {
+                        DispatchQueue.main.async {
+                            self?.imageArray
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
 }
 
-extension UIViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+extension PhotoesViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
       if let pickedImage = info[.originalImage] as? UIImage {
-          FileStorage.saveImage(pickedImage, withName: "some photo")
+          imageArray += [pickedImage]
+          FileStorage.saveImage(pickedImage)
+          self.collectionView?.reloadData()
       }
       picker.dismiss(animated: true, completion: nil)
   }
